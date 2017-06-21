cff418c10c688f93575a15d5ece4e5900ef2e1b7
Filippo Squillace <squillace.filippo@gmail.com>
2017-06-21 17:54:44 +0200

    Set TERM variable in tmux session and fix the Home/End keys

--- pearl-metadata/config.fish
+++ pearl-metadata/config.fish
@@ -1,3 +1,13 @@
 set -x DOT_TMUX_PKGDIR $PEARL_PKGDIR
 
+if [ -n "$TMUX" ]
+    # set TERM according to the parent terminal's TERM (see etc/tmux.conf)
+    switch $TERM
+        case '*-256color'
+            set -x TERM xterm-256color
+        case '*'
+            set -x TERM screen
+    end
+end
+
 # vim: ft=sh
