96226105c3a4d1ec600480671e16a4d92a5c0968
Filippo Squillace <squillace.filippo@gmail.com>
2019-09-03 22:35:59 +1000

    Add optional status bar for linux/osx

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -2,6 +2,20 @@
 post_install() {
     link_to_path "$PEARL_PKGDIR/bin/buzz"
     link tmux "$PEARL_PKGDIR/tmux.conf"
+
+    if ask "Do you want to setup Tmux status bar for dot-tmux?" "N"
+    then
+        link tmux "$PEARL_PKGDIR/tmux-statusbar-common.conf"
+
+        if osx_detect
+        then
+            link tmux "$PEARL_PKGDIR/tmux-statusbar-osx.conf"
+        else
+            link tmux "$PEARL_PKGDIR/tmux-statusbar-linux.conf"
+        fi
+    else
+        _unlink_statusbar
+    fi
 }
 
 post_update() {
@@ -11,4 +25,12 @@ post_update() {
 pre_remove() {
     unlink_from_path "$PEARL_PKGDIR/bin/buzz"
     unlink tmux "$PEARL_PKGDIR/tmux.conf"
+    _unlink_statusbar
 }
+
+_unlink_statusbar() {
+    unlink tmux "$PEARL_PKGDIR/tmux-statusbar-common.conf"
+    unlink tmux "$PEARL_PKGDIR/tmux-statusbar-linux.conf"
+    unlink tmux "$PEARL_PKGDIR/tmux-statusbar-osx.conf"
+}
+
